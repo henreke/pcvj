b48a3d95c0b6c609ca7bd5b4f800f27ba00a71b4
kbqk <kbqk@MI00262561.petrobras.biz>
2017-10-06 18:43:18 -0300

    commit

--- pcvj/src/application/TesteComandos.java
+++ pcvj/src/application/TesteComandos.java
@@ -5,24 +5,30 @@ import comunicacaoJava.ComunicacaoTCP;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.shape.Rectangle;
+import processo.Tanque;
 
 public class TesteComandos {
-	
+
 	@FXML
 	private TextField temp1;
-	
+
+	@FXML
+	private Rectangle tq1;
+
 	private MainApp mainApp;
-	
+
 	public TesteComandos() {}
-	
+
 	@FXML
 	private void initialize() {}
-	
+
+
 	public void setMainApp(MainApp mainApp) {
-		
+
 		this.mainApp = mainApp;
 	}
-	
+
 	@FXML
 	private void enviarPergunta() {
 		ComunicacaoTCP comunicacao = new ComunicacaoTCP("192.168.25.117", 1188);
@@ -31,7 +37,13 @@ public class TesteComandos {
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} 
+		}
+	}
+	
+	@FXML
+	private void encherTq(){
+		
+		Tanque.encher(tq1,tq1.getHeight()+10);
 	}
 
 }
--- pcvj/src/processo/Tanque.java
+++ pcvj/src/processo/Tanque.java
@@ -1,7 +1,10 @@
 package processo;
 
+import java.util.ArrayList;
 import java.util.Timer;
 import java.util.TimerTask;
+
+import javafx.scene.shape.Rectangle;
 
 public class Tanque {
 
@@ -9,17 +12,55 @@ public class Tanque {
 	private float level;
 	private int tempoAquecimento, tempoDecorridoAquecimento;
 	private Timer timer;
+
+	private ArrayList<RampaAquecimento> rampa;
+	private int rampaAtual = 0;
+
 	public void aquecer(int tempo, float temperatura){
+		tempoDecorridoAquecimento = 0;
+		tempoAquecimento = tempo;
 		timer = new Timer();
         timer.schedule(new relogio(), 1000);
 
 	}
 
+	public void aquecer(RampaAquecimento rampaquecer){
+
+		tempoDecorridoAquecimento = 0;
+		tempoAquecimento = rampaquecer.getTempo();
+		timer = new Timer();
+		timer.schedule(new RelogioRampa(), 1000);
+	}
+
 	public void pararAquecer(){}
+
+	public void addRampaAquecimento(int tempo, float temperatura){
+
+		rampa.add(new RampaAquecimento(tempo,temperatura));
+	}
+
+	public void removeRampaAquecimento(int index){
+		rampa.remove(index);
+	}
+
+	public void startRampaAquecimento(){
+
+		if (rampa.isEmpty())
+			return;
+		while (rampa.get(0).isFinished())
+			rampaAtual++;
+
+
+
+	}
 
 	public void drenar(){}
 
-	public void encher(){}
+	public static void encher(Rectangle tq1,double tempo){
+
+		tq1.setHeight(tempo);
+		tq1.setY(tq1.getY()-10);
+	}
 
 	public float getLevel(){
 		return 0;
@@ -45,6 +86,66 @@ public class Tanque {
 		}
 
 
+	}
+
+	class RelogioRampa extends TimerTask{
+
+
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			tempoDecorridoAquecimento++;
+			if (tempoDecorridoAquecimento > tempoAquecimento)
+			{
+				timer.cancel();
+				rampa.get(rampaAtual).finish();
+				rampaAtual++;
+				if (rampaAtual >= rampa.size())
+					pararAquecer();
+
+				else
+					aquecer(rampa.get(rampaAtual));
+
+			}
+		}
+
+
+	}
+
+	class RampaAquecimento{
+		private int tempo;
+		private float temperatura;
+		private boolean finished;
+
+
+		public RampaAquecimento(int tempo, float temperatura){
+			this.tempo = tempo;
+			this.temperatura = temperatura;
+		}
+
+		public RampaAquecimento(){}
+
+
+		public void setTempo(int tempo){
+			this.tempo = tempo;
+		}
+		public void setTemperatura(float temperatura){
+			this.temperatura = temperatura;
+		}
+
+		public int getTempo(){
+			return tempo;
+		}
+		public float getTemperatura(){
+			return temperatura;
+		}
+
+		public boolean isFinished(){
+			return finished;
+		}
+		public void finish(){
+			finished = true;
+		}
 	}
 
 
